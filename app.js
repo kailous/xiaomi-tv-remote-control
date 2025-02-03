c8f300fa575594da0f9fb48d25ba73180fbdfc30
Li Peng <kailous@live.cn>
2025-02-03 12:13:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-// app.js
 const express = require('express');
-const app = express();
 const path = require('path');
 
+const app = express();
+
 // 引入路由
-const scanRoutes = require('./routes/scan');
-const tvRoutes = require('./routes/tv');
+const scanRoutes = require('../routes/scan');
+const tvRoutes = require('../routes/tv');
 
 // 设置静态文件目录，并将默认的 index 文件设置为 scan.html
-app.use(express.static(path.join(__dirname, 'public'), { index: 'scan.html' }));
+app.use(express.static(path.join(__dirname, '../public'), { index: 'scan.html' }));
 
 // 使用路由
 app.use('/', scanRoutes);
 app.use('/', tvRoutes);
 
 // 处理所有未匹配的路由，重定向到错误页面
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.redirect('/error.html?error=页面未找到');
 });
 
-// 启动服务器
-const PORT = process.env.PORT || 80;
-app.listen(PORT, () => {
-  console.log(`服务器正在运行在端口 ${PORT}`);
-});
+// 导出 Express 应用
+module.exports = app;
