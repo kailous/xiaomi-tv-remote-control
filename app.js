8a3dec21522476c3deb0f23c3780bf61192490ef
Li Peng <kailous@live.cn>
2025-02-03 11:57:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 // 启动服务器
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`服务器正在运行在端口 ${PORT}`);
 });
